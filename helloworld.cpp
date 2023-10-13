500731d4857f16ab1f9317d8325e47a99a273465
benT034 <ba.tucker@student.avans.nl>
2023-10-13 11:43:39 +0200

    std out toegevoegd

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,6 +6,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "Hallo wereld!\n";
+    std::cout << "Hallo saus\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
